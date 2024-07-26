138c051beb6e01ee8b5ab35439bd67ba905e4812
saranrajmcse <saranrajmcse@gmail.com>
2024-07-26 14:16:47 +0530

    initial commit

--- functions/videoHandler.js
+++ functions/videoHandler.js
@@ -44,7 +44,7 @@ exports.handler = async (event) => {
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <title>${videoName}</title>
-              <link rel="stylesheet" href="/styles.css">
+              <link rel="stylesheet" href="/public/styles.css">
             </head>
             <body>
               <div class="container">

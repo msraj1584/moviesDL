824be09e4e4920984d56f251707527eb44cf8f4e
saranrajmcse <saranrajmcse@gmail.com>
2024-07-26 14:50:34 +0530

    initial commit

--- functions/videoHandler.js
+++ functions/videoHandler.js
@@ -43,7 +43,7 @@ exports.handler = async (event) => {
             <head>
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>M S RAJ Movies</title>
+              <title>${videoName}</title>
               <style>
               body {
     font-family: Arial, sans-serif;

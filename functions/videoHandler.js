bfae1ae606642a0bdd68360fbb3c453194e78944
saranrajmcse <saranrajmcse@gmail.com>
2024-07-26 12:11:26 +0530

    initial commit

---
+++ functions/videoHandler.js
@@ -0,0 +1,58 @@
+const Seedr = require('seedr');
+const seedr = new Seedr();
+
+const username = process.env.SEEDR_USERNAME;
+const password = process.env.SEEDR_PASSWORD;
+
+exports.handler = async (event) => {
+  const videoId = event.path.split('/').pop(); // Extract videoId from URL
+
+  try {
+    // Login to Seedr
+    await seedr.login(username, password);
+
+    // Fetch video details from Seedr
+    const videoContents = await seedr.getVideos();
+    
+    let videoUrl = null;
+    let videoName = null;
+
+    for (const videoArray of videoContents) {
+      for (const vide of videoArray) {
+        if (vide && vide.id && vide.name && vide.id.toString() === videoId) {
+          try {
+            const downloadLink = await seedr.getFile(vide.id); // Fetch the download link
+            videoUrl = downloadLink.url;
+            videoName = vide.name;
+            break; // Exit loop once the video is found
+          } catch (error) {
+            console.error(`Failed to fetch download link for video ID ${vide.id}:`, error);
+          }
+        }
+      }
+      if (videoUrl) break; // Exit outer loop if video is found
+    }
+    
+    // Return the video URL or a 404 response if not found
+    if (videoUrl) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ videoUrl, videoName }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: 'Video not found',
+      };
+    }
+  } catch (error) {
+    console.error('Error fetching video:', error);
+    return {
+      statusCode: 500,
+      body: 'Internal server error',
+    };
+  }
+};

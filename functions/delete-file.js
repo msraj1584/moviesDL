615fe0fb360eeb7cb03a178b22e9ea691d70e383
saranrajmcse <saranrajmcse@gmail.com>
2025-03-17 18:39:40 +0530

    improvements

--- functions/delete-file.js
+++ functions/delete-file.js
@@ -1,7 +1,8 @@
 const Seedr = require('../seedr');
+const { MongoClient } = require('mongodb');
 const seedr = new Seedr();
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
     if (event.httpMethod !== 'DELETE') {
         return {
             statusCode: 405,
@@ -10,20 +11,35 @@ exports.handler = async function(event, context) {
     }
 
     try {
-        const { id } = JSON.parse(event.body);
-        if (!id) {
+        const { fid,id } = JSON.parse(event.body);
+        if (!fid) {
             return {
                 statusCode: 400,
                 body: JSON.stringify({ error: 'ID is required' }),
             };
         }
-
+        // Seedr credentials
         const username = process.env.SEEDR_USERNAME;
         const password = process.env.SEEDR_PASSWORD;
 
-        await seedr.login(username, password);
-        await seedr.deleteFolder(id);
 
+        await seedr.login(username, password);
+        await seedr.deleteFolder(fid);
+
+        // MongoDB Atlas connection
+        const uri = process.env.MONGODB_URI; // Your MongoDB connection string
+        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        await client.connect();
+
+        const database = client.db('msrajmoviesdldb'); // Replace with your database name
+        const collection = database.collection('msrajmoviesdlcol'); // Replace with your collection name
+        const result = await collection.deleteOne({ id: id });
+        
+
+        // Close MongoDB connection
+        await client.close();
+       
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'File deleted successfully.' }),
